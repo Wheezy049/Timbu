ed4a9353aff0e343296928725e8f97f4142bf2e7
Dev_Faruq <olatoyesefaruq@gmail.com>
2024-07-07 20:29:06 +0100

    HNG stage two ecommerce task

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { FaCartArrowDown, FaUser } from 'react-icons/fa';
+import { FaCartArrowDown } from 'react-icons/fa';
 
 function NavBar() {
   return (
-    <div className=' my-10 mx-24 flex justify-between align-middle'>
+    <div className=' my-10 mx-24 flex justify-between items-center'>
         <div className='bg-white p-2 rounded shadow'>
-            <p className='p-2 text-xs'><span>ROLA`S</span> <br /> STORE</p>
+            <p className='p-2 text-xl font-semibold'>TIMBU`S STORE</p>
         </div>
-        <div className=''>
+        {/* <div className=''>
             <ul className='flex gap-9'>
                 <li>Home</li>
                 <li>Shop</li>
                 <li>About Us</li>
             </ul>
-        </div>
-        <div className='flex gap-4'>
-            <FaCartArrowDown />
-            <FaUser  />
+        </div> */}
+        <div className='flex items-center gap-2'>
+            <p className='text-xl font-bold'>Cart</p>
+            <FaCartArrowDown style={{height: '20px', width: '20px'}} />
         </div>
     </div>
   )
